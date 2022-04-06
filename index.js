1ee3c957ad558ed7901a52d7434474cf0bac87ff
Camila Cristina <criscamila09@gmail.com>
2022-04-06 11:42:49 -0300

    get

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const app = express();
 app.listen('3000') // fica ouvindo o navegador e p entrar na porta definida 
 
 app.route('/').get((req, res) => res.send("Hello"))
+
+app.route('/sobre').get((req, res) => res.send("Hello sobre"))
+

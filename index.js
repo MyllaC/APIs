b32230e95d00509f7e8d95e8598a92ef84971671
Camila Cristina <criscamila09@gmail.com>
2022-04-06 12:11:31 -0300

    PUT

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ const app = express();
 
 app.listen('3000') // fica ouvindo o navegador e p entrar na porta definida 
 
-app.route('/').get((req, res) => res.send("Hello"))
+
+//GET
+app.route('/').get((req, res) => res.send(author))
 
 app.route('/sobre').get((req, res) => res.send("Hello sobre"))
 
+
+//middleware
+app.use(express.json())
+
+
+//POST
+app.route('/').post((req, res) => res.send(req.body))
+
+//PUT
+let author = "Camila"
+
+app.route('/').put((req, res) => {
+  author = req.body.author
+  res.send(author)
+})
